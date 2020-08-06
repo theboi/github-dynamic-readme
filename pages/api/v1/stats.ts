29dcce7be73e8802aea0ffab87ed670ffd674609
Ryan The <ryan.the.2006@gmail.com>
2020-08-06 18:49:51 +0800

    ✨ Add image update on path change

--- pages/api/v1/stats.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.writeHead(200, { "Content-Type": "image/png" });
-  const img = fs.readFileSync('./public/image.png');
-  res.end(img);
-};
