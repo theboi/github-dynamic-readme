f54c7b4dc2d2a348d266e020abafa1d794c7c620
Ryan The <ryan.the.2006@gmail.com>
2020-08-07 22:29:19 +0800

    ✨ Add svg templating unescaping

--- pages/api/stats/index.ts
+++ pages/api/stats/index.ts
@@ -11,20 +11,20 @@ const TEMPLATES = {
 };
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    query: {
-      field = "[]"
-    }
-  } = req
+  const { query } = req;
 
   res.writeHead(200, { "Content-Type": "image/svg+xml" });
-  const template = fs.readFileSync(`${process.cwd()}/public/templates/stats.svg`).toString();
-  const svg = template.replace(/\${\w{0,}}/g, (match) => {
-    const key = match.slice(2).slice(0, -1);
-    return "hello";
-  });
+  // process.cwd() instead of __dirname for absolute path: https://nextjs.org/docs/basic-features/data-fetching#reading-files-use-processcwd
+  const template = fs
+    .readFileSync(`${process.cwd()}/public/templates/stats.svg`)
+    .toString();
+  const svg = template
+    .replace(/\${\w{0,}}/g, (match) => {
+      const key = match.slice(2).slice(0, -1);
+      return (query?.[key] as string) ?? "ERROR";
+    })
+    .replace(/(\\\\|\\{|\\}|\\\$)/g, (match) => match.slice(-1)); // svg templating unescaping
   const img = Buffer.from(svg);
   // const img = fs.readFileSync(`${process.cwd()}/public/templates/stats.svg`);
-  // Use process.cwd() instead of __dirname: https://nextjs.org/docs/basic-features/data-fetching#reading-files-use-processcwd
   res.end(img);
 };
